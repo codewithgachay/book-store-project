7f77434bdf09be63164c894b2ad9b628b7ce68ca
FNarmin <n_farzaliyeva@mail.ru>
2023-10-23 15:55:51 +0400

    index.css & index.html have been modified. New file-index.js & images - books-img.png & X-popup.png have been included.

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+var joinUs = document.querySelector(".join-us");
+var userIcon = document.querySelector(".user-icon");
+var popUp = document.querySelector("#popup");
+joinUs.addEventListener("click", function() {
+    popUp.classList.remove("close-popup");
+  popUp.classList.add("popup");
+});
+var closePopUp = document.querySelector("#popup img");
+closePopUp.addEventListener("click", function(){
+    popUp.classList.remove("popup");
+    popUp.classList.add("close-popup");
+});
+userIcon.addEventListener("click", function() {
+    popUp.classList.remove("close-popup");
+  popUp.classList.add("popup");
+});
