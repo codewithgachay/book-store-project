af837feeb861cd002a32f58aecbf9cc5b4911c72
Gachay Aliyev <codewithgachay@gmail.com>
2023-10-27 11:37:34 +0400

    This is some improvement about folder structure of the project

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -9,6 +9,27 @@ import {
   } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
 
   import { db } from "./firebase.js";
+  //SWIPER
+  var swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on: {
+      resize: function () {
+        swiper.changeDirection(getDirection());
+      },
+    },
+  });
+
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
 
   //VARIABLES
   let sold =0;
