21c734da95a1ad41c2de30bb016f9274041bf328
Gachay Aliyev <codewithgachay@gmail.com>
2023-10-28 13:42:55 +0400

    The catalog files has changes

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,42 +1,13 @@
 //ALL IMOPTRED JAVASCRIPT FILES
 
 import {
-    ref,
-    set,
-    onValue,
-    get,
-    push
-  } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+  ref,
+  set,
+  onValue,
+  get,
+  push,
+} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
 
-  import { db } from "./firebase.js";
-  //SWIPER
-  var swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    direction: getDirection(),
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    on: {
-      resize: function () {
-        swiper.changeDirection(getDirection());
-      },
-    },
-  });
+import { db } from "./firebase.js";
 
-  function getDirection() {
-    var windowWidth = window.innerWidth;
-    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-
-    return direction;
-  }
-
-  //VARIABLES
-  let sold =0;
-  let allBooks =[];
- 
-
-  //DOM ELEMENTS
-  let newReleases;
-
-
+//Variables
